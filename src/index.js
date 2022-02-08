b56b51a7b7ff934b55eaba7fd66f334209052792
gregmcknight <gmcknight23@gmail.com>
2022-02-07 22:04:58 -0500

    greg-mcknight

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import './index.less'
-import './components/article/article.js'
-import './components/menu/menu.js'
+import "./index.less";
+import "./components/article/article.js";
+import "./components/menu/menu.js";
+
+function menuMaker(LinksArray) {
+  const menuWrapper = document.createElement("div");
+  const menuList = document.createElement("ul");
+
+  menuWrapper.appendChild(menuList);
+
+  menuWrapper.classList.add("menu");
+
+  LinksArray.forEach((linkText) => {
+    const link = document.createElement("li");
+    link.textContent = linkText;
+    menuList.appendChild(link);
+  });
+
+  const hamMenu = document.querySelector(".menu-button");
+
+  hamMenu.addEventListener("click", () => {
+    menuWrapper.classList.toggle("menu--open");
+  });
+
+  return menuWrapper;
+}
+
+console.log(menuMaker(menuItems));
